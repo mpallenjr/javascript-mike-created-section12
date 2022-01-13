0292729abef6daed424f3bff7ba260cc2c1c49da
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2022-01-13 09:39:56 -0600

    adding notes from section 12 in correct file.  will update with assignment still.

---
+++ assignments.js
@@ -0,0 +1,12 @@
+
+/*
+// Assignment 1
+let country = 'United States of America'
+let continent = 'North America'
+let population = "330,000,000"
+
+console.log(country, continent, population)
+console.log(country)
+console.log(continent)
+console.log(population)
+*/
